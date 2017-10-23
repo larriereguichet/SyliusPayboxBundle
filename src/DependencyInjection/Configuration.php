ac3baa7483b5f3fc7da0804337d86ac1dec75e0f
PapsOu <vivien.frasca@libre-informatique.fr>
2017-10-23 14:11:27 +0200

    Change DI configuration key name

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('paybox');
+        $rootNode = $treeBuilder->root('librinfo_sylius_paybox');
 
         return $treeBuilder;
     }
